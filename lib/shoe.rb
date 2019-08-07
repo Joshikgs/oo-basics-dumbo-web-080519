652757c8070dc5c20935614268054c5ce6226407
Josh Nho <joshnho@gmail.com>
2019-08-07 03:05:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,1 @@
 # Make your shoe class here!
-class Book
-  def initialize(brand)
-    @brand = brand
-  end
-  
-  attr_reader :brand
-  
-  attr_accessor :color, :size, :material, :condition
-  
-  def cobble
-    puts "Flipping the page...wow, you read fast!"
-  end
-end
-
